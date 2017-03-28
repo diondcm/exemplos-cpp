8da7c216335068620b1c7a06238407ebb067f35d
diondcm <diondcm@gmail.com>
2017-03-28 14:24:27 -0300

    Projeto para implementação da FFT

---
+++ fft/FormPrincipal.h
@@ -0,0 +1,52 @@
+//---------------------------------------------------------------------------
+
+#ifndef FormPrincipalH
+#define FormPrincipalH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+#include <Data.Bind.Components.hpp>
+#include <Data.Bind.DBScope.hpp>
+#include <Data.DB.hpp>
+#include <FireDAC.Comp.Client.hpp>
+#include <FireDAC.Comp.DataSet.hpp>
+#include <FireDAC.DApt.Intf.hpp>
+#include <FireDAC.DatS.hpp>
+#include <FireDAC.Phys.Intf.hpp>
+#include <FireDAC.Stan.Error.hpp>
+#include <FireDAC.Stan.Intf.hpp>
+#include <FireDAC.Stan.Option.hpp>
+#include <FireDAC.Stan.Param.hpp>
+#include <FMXTee.Chart.hpp>
+#include <FMXTee.Engine.hpp>
+#include <FMXTee.Procs.hpp>
+#include <FMXTee.Series.hpp>
+#include <FMX.Memo.hpp>
+#include <FMX.ScrollBox.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *Button1;
+	TChart *Chart1;
+	TLineSeries *Series1;
+	TChart *Chart2;
+	TSplitter *Splitter1;
+	TFDMemTable *memTempo;
+	TFDMemTable *memFreq;
+	TBindSourceDB *BindSourceDB1;
+	TFastLineSeries *Series2;
+	TMemo *Memo1;
+	void __fastcall Button1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
